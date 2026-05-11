d6d5aa7eecc3e03b5f9207d1a934ffe12a0d288f
Vishal Mangla <76559165+vishal-0070@users.noreply.github.com>
2026-05-11 17:29:25 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
+#from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain_openai import ChatOpenAI
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
